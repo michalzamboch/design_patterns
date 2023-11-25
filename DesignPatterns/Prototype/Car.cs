d7d921a3c254e4b3096e4c0cdf97b3da3d82be07
Michal Žamboch <zamboch.michal@seznam.cz>
2023-11-25 23:09:00 +0100

    Prototype.

---
+++ DesignPatterns/Prototype/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    internal class Car : IPrototype
+    {
+        private readonly string brand;
+        private readonly string name;
+        private readonly int topSpeed;
+        private readonly int horsePower;
+
+        public Car(string brand, string name, int topSpeed, int horsePower)
+        {
+            this.brand = brand;
+            this.name = name;
+            this.topSpeed = topSpeed;
+            this.horsePower = horsePower;
+        }
+
+        public Car Clone()
+        {
+            return new Car(brand, name, topSpeed, horsePower);
+        }
+
+        public override string ToString()
+        {
+            return $"{brand} {name} top speed: {topSpeed} hp: {horsePower}";
+        }
+    }
+}
---
+++ DesignPatterns/Prototype/Program.cs
@@ -0,0 +1,19 @@
+﻿using Prototype;
+
+Console.Title = "Builder";
+
+
+Car audi = new("Audi", "A6", 250, 500);
+Car anotherAudi = audi.Clone();
+
+Console.WriteLine(audi.ToString());
+Console.WriteLine(anotherAudi.ToString());
+
+if (audi.Equals(anotherAudi))
+{
+    Console.WriteLine("Equal cars.");
+}
+else
+{
+    Console.WriteLine("Not equal cars.");
+}
