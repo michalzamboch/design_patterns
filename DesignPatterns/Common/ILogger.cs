f52c46ee344840ca495bf89da7f286cb151a5016
Michal Žamboch <zamboch.michal@seznam.cz>
2023-11-28 22:14:25 +0100

    Mock logger

---
+++ DesignPatterns/Common/ILogger.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Common
+{
+    public interface ILogger
+    {
+        void LogInfo(string message);
+        void LogWarning(string message);
+        void LogError(string message);
+    }
+}
---
+++ DesignPatterns/Common/Logger.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Common
+{
+    public class Logger : ILogger
+    {
+        public void LogError(string message)
+        {
+            Console.WriteLine("Error: " + message);
+        }
+
+        public void LogInfo(string message)
+        {
+            Console.WriteLine("Info: " + message);
+        }
+
+        public void LogWarning(string message)
+        {
+            Console.WriteLine("Warning: " + message);
+        }
+    }
+}
